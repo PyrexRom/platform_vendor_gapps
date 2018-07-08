618c2afac9bfbfa309c0f36f55b20ed51fff82e7
Surge Raval <Surge1223@gmail.com>
2018-07-08 22:33:28 +0000

    gapps: add permissions needed to use newer gapps including gms and pixel migrate

--- gapps.mk
+++ gapps.mk
@@ -91,4 +91,8 @@ PRODUCT_COPY_FILES_OVERRIDES += \
     vendor/gapps/priv-app/CarrierConfig/CarrierConfig.apk:system/priv-app/CarrierConfig/CarrierConfig.apk \
     vendor/gapps/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
 
+# Default priv-app permissions
+PRODUCT_COPY_FILES += \
+    vendor/gapps/etc/permissions/privapp-permissions-taimen.xml:system/etc/permissions/privapp-permissions-taimen.xml
+
 $(call inherit-product, vendor/gapps/gapps-blobs.mk)
